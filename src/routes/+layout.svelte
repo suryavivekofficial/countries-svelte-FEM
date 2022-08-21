45294d17f2c657765dc8ca129f696c4eb28f1fd6
surya vivek <suryavivek33377@gmail.com>
2022-08-21 15:52:35 +0530

    Nav bar completed with dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
+	import Nav from '../lib/components/Nav.svelte';
+	import { isDarktheme } from '../lib/stores/themeStore';
 	import '../app.css';
 </script>
 
-<slot />
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div class="{$isDarktheme ? `dark` : ``} h-screen w-screen">
+	<div class="h-full w-full bg-lightGray text-darkBlueText dark:bg-darkBlueBg dark:text-white">
+		<Nav />
+		<slot />
+	</div>
+</div>
+
+<style>
+	div {
+		font-family: 'Nunito Sans', sans-serif;
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			darkBlue: 'hsl(209, 23 %, 22 %)',
+			darkBlueBg: 'hsl(207, 26%, 17%)',
+			darkBlueText: 'hsl(200, 15%, 8%)',
+			darkGray: 'hsl(0, 0%, 52%)',
+			lightGray: 'hsl(0, 0%, 98%)',
+			white: 'hsl(0, 0%, 100%)'
+		}
 	},
 	plugins: []
 };
